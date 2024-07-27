33bb41ac2b4984e481301e0972e6899880b7d5f4
a39849686 <a0967067696@gmail.com>
2024-07-27 19:24:43 +0800

    spring boot with redis

--- boot3-02-demo/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ boot3-02-demo/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.atguigu.boot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    StringRedisTemplate redisTemplate;
+
     @GetMapping("/hello")
     public String hello() {
         return "Hello";
+    }
+
+    @GetMapping("/incr")
+    public String incr() {
+        Long haha = redisTemplate.opsForValue().increment("haha");
+        return "增加的數值：" + haha;
     }
 }
