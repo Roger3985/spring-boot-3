6edfa78122f1ee6773b26f61e0b71d69d95e2a49
a39849686 <a0967067696@gmail.com>
2024-07-28 16:15:15 +0800

    new practice with spring and logging

--- boot3-03-logging/src/main/java/com/atguigu/logging/Boot303LoggingApplication.java
+++ boot3-03-logging/src/main/java/com/atguigu/logging/Boot303LoggingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Boot303LoggingApplication {
+public class  Boot303LoggingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot303LoggingApplication.class, args);
--- boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
+++ boot3-03-logging/src/main/java/com/atguigu/logging/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.atguigu.logging.controller;
 
+import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +19,14 @@ public class HelloController {
     // Logger logger = LoggerFactory.getLogger(getClass());
 
     @GetMapping("/hello")
-    public String hello() {
-        log.info("哈哈哈，方法進來了");
+    public String hello(String a, String b) {
+        log.trace("trace 日誌......");
+        log.debug("debug 日誌......");
+        // SpringBoot  底層默認的日誌級別 info
+        log.info("info 日誌...... 參數a:{} b:{}", a, b);
+        log.warn("warn 日誌......");
+        log.error("error 日誌......");
+        // log.info("哈哈哈，方法進來了");
         // logger.info("哈哈哈，方法進來了");
         return "hello";
     }
