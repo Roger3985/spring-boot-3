1d2a760e78fb625bd069ed16e365e02fe138ad88
zhengchangsun <a0967067696@gmail.com>
2024-07-29 17:47:19 +0800

    spring-web end.

---
+++ boot3-04-web/src/main/java/com/atguigu/web/biz/UserBizHandler.java
@@ -0,0 +1,7 @@
+package com.atguigu.web.biz;/**
+ * @author 2407009
+ * @Description
+ * @create 2024-07-29
+ */
+public class UserBizHandler {
+}
---
+++ boot3-04-web/src/main/java/com/atguigu/web/config/WebFunctionConfig.java
@@ -0,0 +1,7 @@
+package com.atguigu.web.config;/**
+ * @author 2407009
+ * @Description
+ * @create 2024-07-29
+ */
+public class WebFunctionConfig {
+}
