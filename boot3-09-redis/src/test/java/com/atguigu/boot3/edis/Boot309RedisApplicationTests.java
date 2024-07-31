0bd67b07523f8fda11dab7176064011e0dcb32c0
a39849686 <a0967067696@gmail.com>
2024-07-31 21:24:36 +0800

    new project on springboot with redis

---
+++ boot3-09-redis/src/test/java/com/atguigu/boot3/edis/Boot309RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot3.edis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot309RedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
