3292ac4818054e780952652862eddbe4c1c708bc
zhengchangsun <a0967067696@gmail.com>
2024-08-01 12:31:57 +0800

    new project on message(kafka) with springboot 3 web..

---
+++ boot3-12-message/src/test/java/com/atguigu/boot3/message/Boot312MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.boot3.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot312MessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
