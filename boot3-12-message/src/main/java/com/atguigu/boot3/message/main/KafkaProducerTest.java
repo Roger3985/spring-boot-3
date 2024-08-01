9564f7e2a880b42c8c95cc438a65c6d202944d3b
zhengchangsun <a0967067696@gmail.com>
2024-08-01 16:49:55 +0800

    bug on spring with kafka is fixed.

--- boot3-12-message/src/main/java/com/atguigu/boot3/message/main/KafkaProducerTest.java
+++ boot3-12-message/src/main/java/com/atguigu/boot3/message/main/KafkaProducerTest.java
@@ -10,7 +10,7 @@ import java.util.Properties;
 public class KafkaProducerTest {
     public static void main(String[] args) {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "0.0.0.0:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
