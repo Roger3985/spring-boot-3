48b8a2e70ff2dd4b50d91ab49af219d18aad025a
a39849686 <a0967067696@gmail.com>
2024-08-02 07:15:43 +0800

    new version

--- boot3-14-actuator/src/main/java/com/atguigu/boot3/actuator/Boot314ActuatorApplication.java
+++ boot3-14-actuator/src/main/java/com/atguigu/boot3/actuator/Boot314ActuatorApplication.java
@@ -3,6 +3,12 @@ package com.atguigu.boot3.actuator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 整合 Prometheus + Grafana 完成線上應用指標監控系統
+ * 1. 改造 SpringBoot 應用，產生 Prometheus 需要的格式資料
+ *    - 導入 micrometer-registry-prometheus
+ * 2.
+ */
 @SpringBootApplication
 public class Boot314ActuatorApplication {
 
