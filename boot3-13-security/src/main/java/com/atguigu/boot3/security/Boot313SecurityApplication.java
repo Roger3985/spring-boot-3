fd7ebd31d96a7f4d6005ccf320d00aa3473fb918
zhengchangsun <a0967067696@gmail.com>
2024-08-01 18:02:00 +0800

    new version on practice

--- boot3-13-security/src/main/java/com/atguigu/boot3/security/Boot313SecurityApplication.java
+++ boot3-13-security/src/main/java/com/atguigu/boot3/security/Boot313SecurityApplication.java
@@ -3,6 +3,41 @@ package com.atguigu.boot3.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Security 場景的自動配置類:
+ * SecurityAutoConfiguration、SpringBootWebSecurityConfiguration、SecurityFilterAutoConfiguration
+ * 1. security 的所有配置在 SecurityProperties: 以 spring.security 開頭
+ * 2. 默認的 SecurityFilterChain 組件:
+ * 	  - 所有請求都需要進行認證 (登入)
+ * 	  - 開啟了表單登入: spring security 提供了一個默認的登入頁，未經登入的所有請求都需要進行登入。
+ * 	  - 也開啟了基於 httpBasic 的驗證登入方式
+ * 	  由此可知
+ * 	  @Configuration(
+ *         proxyBeanMethods = false
+ *     )
+ *     @ConditionalOnDefaultWebSecurity
+ *     static class SecurityFilterChainConfiguration {
+ *         SecurityFilterChainConfiguration() {
+ *         }
+ *
+ *         @Bean
+ *         @Order(2147483642)
+ *         SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+ *             http.authorizeHttpRequests((requests) -> {
+ *                 ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl)requests.anyRequest()).authenticated();
+ *             });
+ *             http.formLogin(Customizer.withDefaults());
+ *             http.httpBasic(Customizer.withDefaults());
+ *             return (SecurityFilterChain)http.build();
+ *         }
+ *     }
+ * 4. @EnableWebSecurity 生效
+ *    - WebSecurityConfiguration 生效: web 安全配置
+ *    - HttpSecurityConfiguration 生效: http 安全規則
+ *    - @EnableGlobalAuthentication 生效: 全局認證生效
+ *    	-> AuthenticationConfiguration: 認證配置
+ * 5.
+ */
 @SpringBootApplication
 public class Boot313SecurityApplication {
 
---
+++ boot3-13-security/src/main/java/com/atguigu/boot3/security/config/AppSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.atguigu.boot3.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * @author 2407009
+ * @Description
+ * @create 2024-08-01
+ */
+@Configuration
+public class AppSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 請求授權
+        http.authorizeHttpRequests(registry -> {
+            registry.requestMatchers("/").permitAll() // 1. 首頁所有人都允許
+                    .anyRequest().authenticated(); // 2. 剩下的任意請求都需要進行認證 (登入)
+        });
+
+        // 表單登入功能
+        // 3. 表單登入功能: 開啟默認的表單登入功能: Spring Security 提供默認的登入頁(沒有這一項寫入，僅僅寫了上面授權功能，會將預設的表單登入禁用)
+        http.formLogin();
+
+        return http.build();
+    }
+}
