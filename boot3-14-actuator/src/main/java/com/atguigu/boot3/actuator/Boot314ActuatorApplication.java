ea83d7dabe938b2c1d332c7fa59a0dd78378da19
zhengchangsun <a0967067696@gmail.com>
2024-08-02 14:06:45 +0800

    new version on practice on kafka

--- boot3-14-actuator/src/main/java/com/atguigu/boot3/actuator/Boot314ActuatorApplication.java
+++ boot3-14-actuator/src/main/java/com/atguigu/boot3/actuator/Boot314ActuatorApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 整合 Prometheus + Grafana 完成線上應用指標監控系統
  * 1. 改造 SpringBoot 應用，產生 Prometheus 需要的格式資料
  *    - 導入 micrometer-registry-prometheus
- * 2.
+ * 2. 部屬 java 應用。在同一個機器內，訪問 http:ip:9999/actuator/prometheus 就能獲得資料
+ *    在外部訪問 http:ip:9999/actuator/prometheus
  */
 @SpringBootApplication
 public class Boot314ActuatorApplication {
