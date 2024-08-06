5a8dbb09c12d0c8f8b5120de418de16ad151412f
zhengchangsun <a0967067696@gmail.com>
2024-08-06 18:17:08 +0800

    new project on webflux with spring boot

---
+++ springboot3-reactor-programming/chapter03-webflux/src/main/java/com/atguigu/webflux/Chapter03WebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter03WebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter03WebfluxApplication.class, args);
+	}
+
+}
---
+++ springboot3-reactor-programming/chapter03-webflux/src/test/java/com/atguigu/webflux/Chapter03WebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03WebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
