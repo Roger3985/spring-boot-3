ea8f41dfca035182dc181d71574a9deeb09c4b70
zhengchangsun <a0967067696@gmail.com>
2024-08-07 18:08:59 +0800

    new practice on spring data r2dbc

--- springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/Chaptero4R2dbcApplication.java
+++ springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/Chaptero4R2dbcApplication.java
@@ -3,6 +3,18 @@ package com.atguigu.r2dbc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Roger
+ * @Description
+ * @create 2024-08-07
+ *
+ * SpringBoot 對 r2dbc 的自動配置
+ * 1. R2dbcAutoConfiguration: 主要配置連接工廠，連接池
+ * 2. R2dbcDataAutoConfiguration
+ *    -> R2dbcEnt
+ * 3. R2dbcRepositoriesAutoConfiguration
+ * 4. R2dbcTransactionManagerAutoConfiguration
+ */
 @SpringBootApplication
 public class Chaptero4R2dbcApplication {
 
--- springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/controller/AuthorController.java
+++ springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/controller/AuthorController.java
@@ -1,9 +1,20 @@
 package com.atguigu.r2dbc.controller;
 
+import com.atguigu.r2dbc.entity.TAuthor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
 /**
  * @author 2407009
  * @Description
  * @create 2024-08-07
  */
+@RestController
 public class AuthorController {
+
+    @GetMapping("/author")
+    public Flux<TAuthor> getAllAuthor() {
+
+    }
 }
