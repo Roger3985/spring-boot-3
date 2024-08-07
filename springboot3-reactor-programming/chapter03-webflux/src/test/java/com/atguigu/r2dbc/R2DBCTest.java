84d21c82ccfb2a34f45c6150022ca0dd1379849a
zhengchangsun <a0967067696@gmail.com>
2024-08-07 17:47:58 +0800

    new version

--- springboot3-reactor-programming/chapter03-webflux/src/test/java/com/atguigu/r2dbc/R2DBCTest.java
+++ springboot3-reactor-programming/chapter03-webflux/src/test/java/com/atguigu/r2dbc/R2DBCTest.java
@@ -1,14 +1,13 @@
 package com.atguigu.r2dbc;
 
 import com.atguigu.webflux.entity.TAuthor;
-import io.r2dbc.spi.Connection;
-import io.r2dbc.spi.ConnectionFactories;
-import io.r2dbc.spi.ConnectionFactory;
-import io.r2dbc.spi.Statement;
+import io.asyncer.r2dbc.mysql.MySqlConnectionConfiguration;
+import io.asyncer.r2dbc.mysql.MySqlConnectionFactory;
+import io.r2dbc.spi.*;
 import org.junit.jupiter.api.Test;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
 
 /**
  * @author 2407009
@@ -31,7 +30,7 @@ public class R2DBCTest {
         // jdbc:postgres://localhost:5432/test
         // 1. 獲取連接工廠
         ConnectionFactory connectionFactory =
-                ConnectionFactories.get("r2dbc:postgres://localhost:5433/test");
+                ConnectionFactories.get("r2dbc:mysql://localhost:3306/test");
 
         // 2. 獲取連接，發送 SQL
         // JDBC: Statement: 封裝 sql 的
@@ -39,8 +38,9 @@ public class R2DBCTest {
         // 3. 資料的發佈者
         Mono.from(connectionFactory.create())
                 .flatMapMany(connection ->
-                                    connection.createStatement("select * from t_author where id=?")
-                                    .bind(0, 1L)
+                                    connection.createStatement("select * from t_author where id=?id and name=?name")
+                                    .bind("id", 1L) // 具名參數的方式
+                                            .bind("name", "張三")
                                     .execute()
                 ).flatMap(result -> {
                     return result.map(readable -> {
@@ -49,5 +49,42 @@ public class R2DBCTest {
                         return new TAuthor(id, name);
                     });
                 }).subscribe(tAuthor -> System.out.println("tAuthor" + tAuthor));
+    }
+
+    @Test
+    void connection2() throws IOException {
+        // Postgres 的相關配置
+        MySqlConnectionConfiguration configuration = MySqlConnectionConfiguration
+                .builder()
+                .host("localhost")
+                .port(3306)
+                .username("root")
+                .password("123456")
+                .database("test")
+                .build();
+
+        // 1. 獲取連接工廠
+        MySqlConnectionFactory connectionFactory = MySqlConnectionFactory.from(configuration);
+
+        // 3. 資料的發佈者
+        Mono.from(connectionFactory.create())
+                .flatMapMany(connection ->
+                        connection.createStatement("select * from t_author where id=?")
+                                .bind(0, 1L)
+                                .execute()
+                ).flatMap(result -> {
+                    return result.map(readable -> {
+                        Long id = readable.get("id", Long.class);
+                        String name = readable.get("name", String.class);
+                        return new TAuthor(id, name);
+                    });
+                })
+                .take(5) // 只取其中五條
+                .subscribe(tAuthor -> System.out.println("tAuthor" + tAuthor));
+
+
+        // 背壓: 不用返回所有東西，基於請求量返回。
+
+        System.in.read();
     }
 }
