1b2805ea574f63e4cbadcd4a2ae1c6a89be8d7a9
zhengchangsun <a0967067696@gmail.com>
2024-08-08 16:19:55 +0800

    new version practice on webflux

--- springboot3-reactor-programming/chaptero4-r2dbc/src/test/java/com/atguigu/r2dbc/R2DBCTest.java
+++ springboot3-reactor-programming/chaptero4-r2dbc/src/test/java/com/atguigu/r2dbc/R2DBCTest.java
@@ -24,7 +24,7 @@ public class R2DBCTest {
     @Autowired
     R2dbcEntityTemplate r2dbcEntityTemplate; // CRUD API // 更多操作範例請參照文檔
 
-    // DatabaseClient 貼近底層，join 操作好做
+    // DatabaseClient 貼近底層，join 操作好做，複雜查詢好用
     @Autowired
     DatabaseClient databaseClient; // 資料庫客戶端
 
@@ -46,6 +46,23 @@ public class R2DBCTest {
         r2dbcEntityTemplate
                 .select(query, TAuthor.class)
                 .subscribe(tAuthor -> System.out.println("tAuthor: " + tAuthor));
+
+        System.in.read();
+    }
+
+    @Test // 底層操作
+    void databaseClient() throws IOException {
+        databaseClient.sql("select * from t_author where id=?id")
+                .bind(0, 2L)
+                .fetch() // 抓取資料
+                .all() // 返回所有
+                .map(map -> { // map == bean 屬性 = 值
+                    System.out.println("map = " + map);
+                    String id = map.get("id").toString();
+                    String name = map.get("name").toString();
+                    return new TAuthor(Long.parseLong(id), name);
+                })
+                .subscribe(tAuthor -> System.out.println("tAuthor = " + tAuthor));
 
         System.in.read();
     }
