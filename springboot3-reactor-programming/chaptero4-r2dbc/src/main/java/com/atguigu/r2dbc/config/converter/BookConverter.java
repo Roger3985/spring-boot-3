3fb3d775e9c6823c1f7bf8ea23d0b832fe156610
a39849686 <a0967067696@gmail.com>
2024-08-08 22:48:09 +0800

    new practice

--- springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/config/converter/BookConverter.java
+++ springboot3-reactor-programming/chaptero4-r2dbc/src/main/java/com/atguigu/r2dbc/config/converter/BookConverter.java
@@ -19,6 +19,11 @@ import java.time.Instant;
 @ReadingConverter // 讀取資料庫資料的時候，把 row 轉成 TBook 把 row 轉成 TBook
 public class BookConverter implements Converter<Row, TBook> {
 
+    /*
+        (1) @Query 指定了 sql 如何發送
+        (2) 自定義 BookConverter 指定了 資料庫返回的一 Row 資料，怎麼封裝成 TBook
+        (3) 配置 R2dbcCustomConversions 組件，讓 BookConverter 加入其中生效
+     */
     @Override
     public TBook convert(Row source) {
         // 自定義結果集封裝
@@ -32,6 +37,6 @@ public class BookConverter implements Converter<Row, TBook> {
         tAuthor.setId(authorId);
         tAuthor.setName(source.get("name", String.class));
         tBook.setAuthor(tAuthor);
-        return null;
+        return tBook;
     }
 }
